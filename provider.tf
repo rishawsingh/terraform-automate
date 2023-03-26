81e58dbc68571922f3e0020c533350607dda6088
Rishaw Kumar <68506932+rishawsingh@users.noreply.github.com>
2023-03-26 17:18:26 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -8,5 +8,10 @@ terraform {
 }
 
 provider "aws" {
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
   region     = "us-east-1"
 }
+
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
